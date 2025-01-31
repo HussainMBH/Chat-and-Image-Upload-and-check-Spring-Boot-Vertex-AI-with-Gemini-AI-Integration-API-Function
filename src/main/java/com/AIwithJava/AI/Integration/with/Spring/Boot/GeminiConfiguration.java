582db22e2bae5428796168f21c247cbac7b262a3
Bahir Hussain <mbahirh624@gmail.com>
2025-01-31 17:25:15 +0530

    add

--- src/main/java/com/AIwithJava/AI/Integration/with/Spring/Boot/GeminiConfiguration.java
+++ src/main/java/com/AIwithJava/AI/Integration/with/Spring/Boot/GeminiConfiguration.java
@@ -8,7 +8,10 @@ import com.google.cloud.vertexai.VertexAI;
 @Configuration(proxyBeanMethods = false)
 public class GeminiConfiguration {
 
-
+    @Bean
+    public VertexAI vertexAI() {
+        return new VertexAI("sodium-wall-429709-r1", "asia-south1");
+    }
 
     @Bean
     public GenerativeModel generativeModel(VertexAI vertexAI) {
